34e144b7a12ef94155d0c2f63c9ba37b320af21e
AnhTNguyen <31691766+anhnt6596@users.noreply.github.com>
2021-10-12 23:02:20 +0700

    mouse 3d

--- Assets/Utils/MousePosition3D.cs
+++ Assets/Utils/MousePosition3D.cs
@@ -6,13 +6,22 @@ public class MousePosition3D : MonoBehaviour
 {
 
     [SerializeField] private Camera mainCamera;
-    [SerializeField] private LayerMask layerMask;
     void Update()
     {
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
+        //if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
+        //{
+        //    transform.position = raycastHit.point;
+        //}
+        // You successfully hi
+        Plane hPlane = new Plane(Vector3.up, Vector3.zero);
+        // Plane.Raycast stores the distance from ray.origin to the hit point in this variable:
+        float distance = 0;
+        // if the ray hits the plane...
+        if (hPlane.Raycast(ray, out distance))
         {
-            transform.position = raycastHit.point;
+            // get the hit point:
+            transform.position = ray.GetPoint(distance);
         }
     }
 }
