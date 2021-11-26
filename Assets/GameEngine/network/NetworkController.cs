01559d1fe3cf27289707f289dde0c87765e8b0f5
QxChi <traitimtanvo3004@gmail.com>
2021-11-26 23:48:24 +0700

    feat: config build

--- Assets/GameEngine/network/NetworkController.cs
+++ Assets/GameEngine/network/NetworkController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Sfs2X;
 using Sfs2X.Util;
 using Sfs2X.Core;
-using Sfs2X.Entities;
+using Sfs2X.Entities; 
 using Sfs2X.Entities.Data;
 using Sfs2X.Requests;
 
