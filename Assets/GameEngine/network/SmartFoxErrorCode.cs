1e0e0884cf99b681997475b7ab13c8e2afdb544f
QxChi <traitimtanvo3004@gmail.com>
2021-11-28 23:30:44 +0700

    feat: add error code

--- Assets/GameEngine/network/SmartFoxErrorCode.cs
+++ Assets/GameEngine/network/SmartFoxErrorCode.cs
@@ -5,4 +5,12 @@
     INVALID_DATA = 2,
     FIGHTING_LINE_LIST_EMPTY = 3,
     SAILOR_LIST_EMPTY = 4,
+    PICK_TEAM_FIGHTING_LINE_LIST_INVALID =	5,	// ft không hợp lệ (sửa tên cho nó dể hiểu lỗi của lệnh nào )
+    COMBAT_FIGHTING_FINISHED = 6,	//Chặn lệnh đúp ( chưa xử lý )
+    SAILOR_CONFIG_INVALID =	7, //Không load được sailor config file
+    COMBAT_SAILOR_AND_FIGHITING_LINE_INVALID = 8,	//sailor và ft không hợp lệ ( kể cả số lượng lẫn id phải khớp nhau )
+    STAMINA_SOLD_OUT = 9,	//hết lượt mua
+    STAMINA_BERI_NOT_ENOUGH = 10,	//ko đủ beri để mua
+    COMBAT_STAMINA_NOT_ENOUGH =	11,	//ko du stamina de choi
+    COMBAT_NOT_PREPARE = 12,	// chua chuan bi
 }
