0f9ffb831c106115530841cea454b8936812a281
s0nw <sonmx@vmodev.com>
2021-12-15 16:04:18 +0700

    done tooltip and fix pve

--- Assets/Resources/Characters/SailorCollider.cs
+++ Assets/Resources/Characters/SailorCollider.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SailorCollider : MonoBehaviour
 {
     private void OnMouseDown()
     {
-        //if (UIIngameMgr.Instance != null) UIIngameMgr.Instance.ShowSailorDetail(gameObject);
+        if (SceneManager.GetActiveScene().name == "ScenePickTeamBattle" && gameObject.transform.parent.name == "SquadA")
+            return;
         if (TooltipSailorInfo.Instance != null) TooltipSailorInfo.Instance.ShowTooltip(gameObject);
     }
 }
--- Assets/Scenes/SceneLobby/script/LobbyUI.cs
+++ Assets/Scenes/SceneLobby/script/LobbyUI.cs
@@ -14,172 +14,179 @@ using Piratera.Sound;
 public class LobbyUI : MonoBehaviour
 {
 
-	//----------------------------------------------------------
-	// UI elements
-	//----------------------------------------------------------
+    //----------------------------------------------------------
+    // UI elements
+    //----------------------------------------------------------
 
-	[SerializeField]
-	private Text userName;
+    [SerializeField]
+    private Text userName;
 
-	[SerializeField]
-	private Image userAvatar;
+    [SerializeField]
+    private Image userAvatar;
 
-	[SerializeField]
-	private Text userBeri;
+    [SerializeField]
+    private Text userBeri;
 
-	[SerializeField]
-	private Text userStamina;
+    [SerializeField]
+    private Text userStamina;
 
-	[SerializeField]
-	private Text userStaminaCountDown;
+    [SerializeField]
+    private Text userStaminaCountDown;
 
 
-	[SerializeField]
-	private Button[] leftButtons;
+    [SerializeField]
+    private Button[] leftButtons;
 
-	[SerializeField]
-	private Button[] rightButtons;
+    [SerializeField]
+    private Button[] rightButtons;
 
-	[SerializeField]
-	private Button buttonAdventure;
+    [SerializeField]
+    private Button buttonAdventure;
 
-	[SerializeField]
-	private LoadAvatarUtils loadAvatarUtils;
+    [SerializeField]
+    private LoadAvatarUtils loadAvatarUtils;
 
-	[SerializeField]
-	private List<Transform> nodeSailors;
+    [SerializeField]
+    private List<Transform> nodeSailors;
 
-	[SerializeField]
-	private Transform buttonCol;
-	[SerializeField]
-	private Transform sail;
-	[SerializeField]
-	private Transform background;
+    [SerializeField]
+    private Transform buttonCol;
+    [SerializeField]
+    private Transform sail;
+    [SerializeField]
+    private Transform background;
 
-	// Start is called before the first frame update
-	void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-		SoundMgr.PlayBGMusic(PirateraMusic.LOBBY);
-		loadAvatarUtils.LoadAvatar(userAvatar, UserData.Instance.Avatar);
-		UpdateUserInfo();
-		GameEvent.UserDataChanged.AddListener(UpdateUserInfo);
-		GameEvent.UserBeriChanged.AddListener(OnBeriChanged);
-		GameEvent.UserStaminaChanged.AddListener(OnStaminaChanged);
-		RunAppearAction();
-		ShowListSailors();
-	}
+        SoundMgr.PlayBGMusic(PirateraMusic.LOBBY);
+        loadAvatarUtils.LoadAvatar(userAvatar, UserData.Instance.Avatar);
+        UpdateUserInfo();
+        GameEvent.UserDataChanged.AddListener(UpdateUserInfo);
+        GameEvent.UserBeriChanged.AddListener(OnBeriChanged);
+        GameEvent.UserStaminaChanged.AddListener(OnStaminaChanged);
+        RunAppearAction();
+        ShowListSailors();
+    }
 
     private void OnBeriChanged(long oldValue, long newValue)
     {
-		DoTweenUtils.UpdateNumber(userBeri, oldValue, newValue, x => StringUtils.ShortNumber(x));
+        DoTweenUtils.UpdateNumber(userBeri, oldValue, newValue, x => StringUtils.ShortNumber(x));
 
-	}
+    }
 
-	private void OnStaminaChanged(int oldValue, int newValue)
-	{
-		DoTweenUtils.UpdateNumber(userStamina, oldValue, newValue, x => UserData.Instance.GetStaminaFormat((int)x));
-	}
-
-	private void OnDestroy()
+    private void OnStaminaChanged(int oldValue, int newValue)
     {
-		GameEvent.UserDataChanged.RemoveListener(UpdateUserInfo);
-		GameEvent.UserBeriChanged.RemoveListener(OnBeriChanged);
-		GameEvent.UserStaminaChanged.RemoveListener(OnStaminaChanged);
-	}
+        DoTweenUtils.UpdateNumber(userStamina, oldValue, newValue, x => UserData.Instance.GetStaminaFormat((int)x));
+    }
+
+    private void OnDestroy()
+    {
+        GameEvent.UserDataChanged.RemoveListener(UpdateUserInfo);
+        GameEvent.UserBeriChanged.RemoveListener(OnBeriChanged);
+        GameEvent.UserStaminaChanged.RemoveListener(OnStaminaChanged);
+    }
 
     void UpdateUserInfo(List<string> changedVars)
     {
-		
-		if (changedVars.Contains(UserInfoPropertiesKey.USERNAME))
+
+        if (changedVars.Contains(UserInfoPropertiesKey.USERNAME))
         {
-			userName.DOText(UserData.Instance.Username.LimitLength(11), 0.5f).SetEase(Ease.InOutCubic);
-		}
-	}
+            userName.DOText(UserData.Instance.Username.LimitLength(11), 0.5f).SetEase(Ease.InOutCubic);
+        }
+    }
 
-	void UpdateUserInfo()
-	{
-		userName.DOText(UserData.Instance.Username.LimitLength(11), 0.5f).SetEase(Ease.InOutCubic);
-		DoTweenUtils.UpdateNumber(userBeri, 0, UserData.Instance.Beri, x => StringUtils.ShortNumber(x));
-		DoTweenUtils.UpdateNumber(userStamina, 0, UserData.Instance.Stamina, x => UserData.Instance.GetStaminaFormat((int)x));
-	}
-
-	public void OnLogoutButtonClick()
-	{
-		GuiManager.Instance.AddGui<GuiSetting>("Prefap/GuiSetting", LayerId.GUI);
-	}
-	public void OnStartNewGameButtonClick()
-	{
-		//NetworkController.Send(SFSAction.COMBAT_PREPARE);
-		NetworkController.Send(SFSAction.PVE_PLAY);
-	}
-	public void OnButtonPickTeamClick()
-	{
-		SceneManager.LoadScene("ScenePickTeam");
-	}
-
-	public void ShowStaminaPack()
+    void UpdateUserInfo()
     {
-		GuiManager.Instance.AddGui<GuiBuyStamina>("Prefap/GuiBuyStamina", LayerId.GUI);
-	}
+        userName.DOText(UserData.Instance.Username.LimitLength(11), 0.5f).SetEase(Ease.InOutCubic);
+        DoTweenUtils.UpdateNumber(userBeri, 0, UserData.Instance.Beri, x => StringUtils.ShortNumber(x));
+        DoTweenUtils.UpdateNumber(userStamina, 0, UserData.Instance.Stamina, x => UserData.Instance.GetStaminaFormat((int)x));
+    }
 
-	public void OnBuyBeri()
+    public void OnLogoutButtonClick()
     {
-		Application.OpenURL("https://piratera.io/");
-	}
-
-	public void ShowCommingSoon()
-	{
-		GuiManager.Instance.ShowPopupNotification("Coming Soon!");
-	}
-
-	public void ShowSceneCrew()
+        GuiManager.Instance.AddGui<GuiSetting>("Prefap/GuiSetting", LayerId.GUI);
+    }
+    public void OnStartNewGameButtonClick(GameObject g)
     {
-		SceneManager.LoadScene("SceneCrew");
-	}
-
-	private void Update()
-	{
-		if (UserData.Instance.IsRecorveringStamina())
-		{
-			TimeSpan remaining = TimeSpan.FromMilliseconds(UserData.Instance.TimeToHaveNewStamina());
-			userStaminaCountDown.text = string.Format("{0:00}:{1:00}:{2:00}", remaining.Hours, remaining.Minutes, remaining.Seconds);
-		} else
+        if (UserData.Instance.GetStamina() < 5)
+            GuiManager.Instance.AddGui<GuiBuyStamina>("Prefap/GuiBuyStamina", LayerId.GUI);
+        else
         {
-			userStaminaCountDown.text = "";
-
-		}
-	}
-
-	private void RunAppearAction()
+            g.SetActive(false);
+            NetworkController.Send(SFSAction.PVE_PLAY);
+        }
+            
+    }
+    public void OnButtonPickTeamClick()
     {
-		for (int i = 0; i < leftButtons.Length; i++)
-		{
-			DoTweenUtils.FadeAppearY(leftButtons[i], -200, 0.4f, 1.2f + i * 0.2f, Ease.OutCirc);
-		}
+        SceneManager.LoadScene("ScenePickTeam");
+    }
 
-		DoTweenUtils.ButtonBigAppear(buttonAdventure, 0.6f, Vector3.one, 0.7f);
+    public void ShowStaminaPack()
+    {
+        GuiManager.Instance.AddGui<GuiBuyStamina>("Prefap/GuiBuyStamina", LayerId.GUI);
+    }
 
-		buttonCol.Translate(200, 0, 0);
-		buttonCol.DOMove(new Vector3(-200, 0, 0), 0.8f).SetRelative().SetEase(Ease.OutCirc);
+    public void OnBuyBeri()
+    {
+        Application.OpenURL("https://piratera.io/");
+    }
 
-		sail.Translate(50, 180, 0);
-		sail.DOMove(new Vector3(-50, -180, 0), 0.8f).SetRelative().SetEase(Ease.OutCirc);
+    public void ShowCommingSoon()
+    {
+        GuiManager.Instance.ShowPopupNotification("Coming Soon!");
+    }
 
-		var scale = new Vector3(0.6f, 0.6f, 0.6f);
-		background.localScale += scale;
-		background.DOScale(-scale, 0.8f).SetRelative().SetEase(Ease.OutCirc);
-	}
-	private void ShowListSailors()
-	{
-		for (int i = 0; i < nodeSailors.Count; i++)
-		{
-			if (i >= CrewData.Instance.Sailors.Count) break;
-			var model = CrewData.Instance.Sailors[i];
-			Instantiate(model.config_stats.model, nodeSailors[i]);
-		}
-	}
-	public void ShowGuiCheat()
-	{
-		GuiManager.Instance.AddGui<PopupCheatGame>("Cheat/PopupCheat");
-	}
+    public void ShowSceneCrew()
+    {
+        SceneManager.LoadScene("SceneCrew");
+    }
+
+    private void Update()
+    {
+        if (UserData.Instance.IsRecorveringStamina())
+        {
+            TimeSpan remaining = TimeSpan.FromMilliseconds(UserData.Instance.TimeToHaveNewStamina());
+            userStaminaCountDown.text = string.Format("{0:00}:{1:00}:{2:00}", remaining.Hours, remaining.Minutes, remaining.Seconds);
+        }
+        else
+        {
+            userStaminaCountDown.text = "";
+
+        }
+    }
+
+    private void RunAppearAction()
+    {
+        for (int i = 0; i < leftButtons.Length; i++)
+        {
+            DoTweenUtils.FadeAppearY(leftButtons[i], -200, 0.4f, 1.2f + i * 0.2f, Ease.OutCirc);
+        }
+
+        DoTweenUtils.ButtonBigAppear(buttonAdventure, 0.6f, Vector3.one, 0.7f);
+
+        buttonCol.Translate(200, 0, 0);
+        buttonCol.DOMove(new Vector3(-200, 0, 0), 0.8f).SetRelative().SetEase(Ease.OutCirc);
+
+        sail.Translate(50, 180, 0);
+        sail.DOMove(new Vector3(-50, -180, 0), 0.8f).SetRelative().SetEase(Ease.OutCirc);
+
+        var scale = new Vector3(0.6f, 0.6f, 0.6f);
+        background.localScale += scale;
+        background.DOScale(-scale, 0.8f).SetRelative().SetEase(Ease.OutCirc);
+    }
+    private void ShowListSailors()
+    {
+        for (int i = 0; i < nodeSailors.Count; i++)
+        {
+            if (i >= CrewData.Instance.Sailors.Count) break;
+            var model = CrewData.Instance.Sailors[i];
+            Instantiate(model.config_stats.model, nodeSailors[i]);
+        }
+    }
+    public void ShowGuiCheat()
+    {
+        GuiManager.Instance.AddGui<PopupCheatGame>("Cheat/PopupCheat");
+    }
 }
