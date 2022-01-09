9fcf6764ba5497932b6273d115b8289c9643f06e
HeartEarthDragon <traitimtanvo3004@gmail.com>
2022-01-09 22:27:19 +0700

    feat: add crashlytics

--- Assets/Scenes/SceneLoading/SceneLoadingUI.cs
+++ Assets/Scenes/SceneLoading/SceneLoadingUI.cs
@@ -10,6 +10,27 @@ public class SceneLoadingUI : MonoBehaviour
     [SerializeField]
     private Text textInfo;
 
+    private void Awake()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                // app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
     private void Start()
     {
         Sequence seq = DOTween.Sequence();
