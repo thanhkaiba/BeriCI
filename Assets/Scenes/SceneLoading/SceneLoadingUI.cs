a3231f81111797b74187353b8f74e435c0ba0436
HeartEarthDragon <traitimtanvo3004@gmail.com>
2022-01-10 08:01:09 +0700

    feat: update lib firebase

--- Assets/Scenes/SceneLoading/SceneLoadingUI.cs
+++ Assets/Scenes/SceneLoading/SceneLoadingUI.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using Piratera.Crashlytics;
 using Piratera.Engine;
 using Piratera.GUI;
 using UnityEngine;
@@ -12,23 +13,7 @@ public class SceneLoadingUI : MonoBehaviour
 
     private void Awake()
     {
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == Firebase.DependencyStatus.Available)
-            {
-                // Create and hold a reference to your FirebaseApp,
-                // where app is a Firebase.FirebaseApp property of your application class.
-                // app = Firebase.FirebaseApp.DefaultInstance;
-
-                // Set a flag here to indicate whether Firebase is ready to use by your app.
-            }
-            else
-            {
-                Debug.LogError(System.String.Format(
-                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-                // Firebase Unity SDK is not safe to use here.
-            }
-        });
+        CrashlyticsInitializer.Instance.CrashlyticsInit();
     }
 
     private void Start()
