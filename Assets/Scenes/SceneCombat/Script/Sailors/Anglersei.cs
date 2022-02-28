2d5f2b71da3bba4c2b7549c6b988b46f8a83b76a
AnhTNguyen <31691766+anhnt6596@users.noreply.github.com>
2022-02-28 23:28:03 +0700

    fix bug

--- Assets/Scenes/SceneCombat/Script/Sailors/Anglersei.cs
+++ Assets/Scenes/SceneCombat/Script/Sailors/Anglersei.cs
@@ -28,10 +28,10 @@ public class Anglersei : CombatSailor
             target.transform.position.z - 0.1f
         );
         Sequence seq = DOTween.Sequence();
-        StartCoroutine(GameUtils.WaitAndDo(0.3f, () => SoundMgr.PlaySoundAttackSailor(0)));
+        StartCoroutine(GameUtils.WaitAndDo(0.45f, () => SoundMgr.PlaySoundAttackSailor(0)));
         seq.AppendInterval(0.1f);
         seq.Append(transform.DOMove(desPos, 0.2f).SetEase(Ease.OutSine));
-        seq.AppendInterval(0.8f);
+        seq.AppendInterval(1.0f);
         seq.Append(transform.DOMove(oriPos, 0.1f).SetEase(Ease.OutSine));
         return 0.8f;
     }
